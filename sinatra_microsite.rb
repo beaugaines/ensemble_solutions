a7cd562591300044923b5cb4a66595aaf8f68811
Charlie Gaines <beaugaines@gmail.com>
2013-10-10 22:13:30 -0400

    back to haml, ahh

--- sinatra_microsite.rb
+++ sinatra_microsite.rb
@@ -1,7 +1,32 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'slim'
 
 class SinatraMicrosite < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  enable :inline_templates
+
+  assets do
+    serve '/js',    from: 'app/js'
+    serve '/css',    from: 'app/css'
+    serve '/image',    from: 'app/image'
+
+    css :cssapp, [
+      '/css/*.css'
+    ]
+
+    js :jsapp, [
+      '/js/*.js'
+      '/js/*/*.js'
+    ]
+
+    css_compression :simple
+    js_compression :simple
+  end
 
   get '/' do
     'Yo mundo'
