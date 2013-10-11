25702b89e67e68da5f79b7dcb11e311f1181862d
Charlie Gaines <beaugaines@gmail.com>
2013-10-11 15:39:26 -0400

    bundled

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', '~>1.4.3', :require => 'sinatra/base'
-gem 'sinatra-assetpack', '~>0.3.1', :require => 'sinatra/assetpack'
 gem 'haml'
 gem 'pony'
 
--- app.rb
+++ app.rb
@@ -1,32 +1,10 @@
 require 'rubygems'
 require 'sinatra/base'
-require 'sinatra/assetpack'
 require 'haml'
+require 'pony'
 
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
-
-  register Sinatra::AssetPack
-
-  enable :inline_templates
-
-  assets do
-    serve '/js',    from: 'app/js'
-    serve '/css',    from: 'app/css'
-    serve '/image',    from: 'app/image'
-
-    css :cssapp, [
-      '/css/*.css'
-    ]
-
-    js :jsapp, [
-      '/js/*.js',
-      '/js/*/*.js'
-    ]
-
-    css_compression :sqwish
-    js_compression :jsmin
-  end
 
   get '/' do
     haml :index
