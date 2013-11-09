2bf6a45890724af80ab485b92ca402321cd1420c
Charlie Gaines <beaugaines@gmail.com>
2013-11-09 14:59:27 -0500

    Pony config for production

--- app.rb
+++ app.rb
@@ -6,6 +6,22 @@ require 'pony'
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
+  configure :production do
+    Pony.options = {
+      :via => :smtp,
+      :via_options => {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :domain => 'heroku.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
+    }
+  end
+
+
   get '/' do
     haml :index
   end
@@ -15,14 +31,8 @@ class App < Sinatra::Base
       from: "#{params[:email]}",
       to: "beaugaines@gmail.com",
       subject: "Ensemble Solutions contact from #{params[:name]}",
-      body: "#{params[:message]}",
-      port: '587',
-      enable_starttls_auto: true,
-      user_name:  ENV['SENDGRID_USERNAME'],
-      password:  ENV['SENDGRID_PASSWORD'],
-      authentication: :plain,
-      domain:  'heroku.com'
-      )
+      body: "#{params[:message]}"
+    )
   end
 
 run! if app_file == $0
